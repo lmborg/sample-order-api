e2003e7cee62a3190c48f868ac02f2c2cd1c249c
Mikael Borg <mikael.borg@thetrainline.com>
2025-11-12 23:54:50 +0000

    fix yml file and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+## Order Api
+
+To run locally and test via Swagger: `dotnet run`
+
+To run integration tests: `dotnet test`
--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,6 +1,10 @@
 using Api.IoC;
 using Application.IoC;
+
+using Infrastructure.Data;
 using Infrastructure.IoC;
+
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,13 +18,20 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
+if (app.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<OrderApiDbContext>();
+    db.Database.Migrate();
+}
+
 app.MapControllers();
 
 app.UseExceptionHandler();
 
 app.Run();
 
-namespace Api
+namespace Api // for integration tests
 {
     public partial class Program;
 }
